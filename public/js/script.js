e1e74c6cc9d1fd806abe6bf3f5d755d3f604d936
Emilio Vasquez <emiliovasquezcarbajalalexander@gmail.com>
2023-08-20 14:34:47 -0400

    structured html changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,11 @@
 document.getElementById("sidebarToggle").addEventListener("click", function() {
     const sidebar = document.getElementById("sidebar");
+    const body = document.querySelector('body');
     if (sidebar.style.left === "-250px") {
         sidebar.style.left = "0px";
+        body.classList.add('sidebar-opened');
     } else {
         sidebar.style.left = "-250px";
+        body.classList.remove('sidebar-opened');
     }
 });
